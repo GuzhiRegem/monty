6774ddfc7cd715b904bee80c2e7f7d81a89d73ba
fedeeblengio <fedeeblengio@gmail.com>
2021-09-21 18:24:53 +0000

    f_pall fun

--- funcs1.c
+++ funcs1.c
@@ -1,11 +1,16 @@
 #include "monty.h"
 
-void f_push(stack_t **stack, unsigned int line_number)
-{
-}
 void f_pall(stack_t **stack, unsigned int line_number)
 {
-}
-void f_pint(stack_t **stack, unsigned int line_number)
-{
+	stack_t *new_node = *stack;
+
+	if (stack != NULL || *stack != NULL)
+        {
+                line_number = line_number;
+                while (new_node != NULL)
+                {
+                        printf("%i\n", new_node->n);
+                new_node = new_node->next;
+                }
+        }
 }
