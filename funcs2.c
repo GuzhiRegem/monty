78b74b2695acd538e544a871fd75ce73fa87d2d8
Guzhi <3292@holbertonschool.com>
2021-09-23 16:14:11 -0700

    commit

--- funcs2.c
+++ funcs2.c
@@ -5,9 +5,17 @@
   *@line_number: line number
   *Return: nothing
   */
-void f_pop(stack_t **stack, __attribute__ ((unused)) unsigned int line_number)
+void f_pop(stack_t **stack, unsigned int line_number)
 {
-	printf("stack: %p l_num: %i", (void *)stack, line_number);
+	stack_t *nex;
+	if (!*stack)
+	{
+		fprintf(stderr, "L%i: can't pop an empty stack\n", line_number);
+		_ex(stack, NULL);
+	}
+	nex = (*stack)->next;
+	free(*stack);
+	*stack = nex;
 }
 /**
   *f_swap - function
