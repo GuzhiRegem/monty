3249c6edeac0bc230e2079c49845a1eba0282c48
Guzhi <3292@holbertonschool.com>
2021-09-23 16:21:35 -0700

    commit

--- funcs2.c
+++ funcs2.c
@@ -23,9 +23,21 @@ void f_pop(stack_t **stack, unsigned int line_number)
   *@line_number: line number
   *Return: nothing
   */
-void f_swap(stack_t **stack, __attribute__ ((unused)) unsigned int line_number)
+void f_swap(stack_t **stack, unsigned int line_number)
 {
-	printf("stack: %p l_num: %i", (void *)stack, line_number);
+	stack_t *first = NULL, *second = NULL, *third = NULL;
+
+	first = *stack;
+	if (first)
+		second = first->next;
+	if (!first || !second)
+		fprintf(stderr, "L%i: can't swap, stack too short\n", line_number);
+	third = second->next;
+	first->prev = second;
+	first->next = third;
+	second->prev = NULL;
+	second->next = first;
+	*stack = second;
 }
 /**
   *f_add - function
@@ -33,7 +45,7 @@ void f_swap(stack_t **stack, __attribute__ ((unused)) unsigned int line_number)
   *@line_number: line number
   *Return: nothing
   */
-void f_add(stack_t **stack, __attribute__ ((unused)) unsigned int line_number)
+void f_add(stack_t **stack, unsigned int line_number)
 {
 	printf("stack: %p l_num: %i", (void *)stack, line_number);
 }
